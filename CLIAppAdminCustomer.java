59c461d68fee452b8c42b26ad67caa17d3274482
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-02-16 20:45:03 +0400

    add main menu and main work flow functionality

--- CLIAppAdminCustomer.java
+++ CLIAppAdminCustomer.java
@@ -4,27 +4,59 @@ import java.util.Scanner;
 public class CLIAppAdminCustomer{
     
     // declear variables
-    ArrayList<ArrayList<String>> tickets = new ArrayList<>();
-    String ticketNumber = "";
-    String ticketDescription = "";
-    String ticketPriority = "";
-    Scanner inputChoise = new Scanner(System.in);
-    Scanner inputTicketInfo = new Scanner(System.in);
-    boolean count = true;
+    static ArrayList<ArrayList<String>> tickets = new ArrayList<>();
+    static String ticketNumber = "";
+    static String ticketDescription = "";
+    static String ticketPriority = "";
+    static Scanner inputChoice = new Scanner(System.in);
+    static Scanner inputTicketInfo = new Scanner(System.in);
+    static String choice = "";
+    static boolean count = true;
 
     public static void main(String[] args) {
 
-        // display main menu and take user input
+        // display program first header
+        System.out.println("=".repeat(50));
+        System.out.println("        Welcom to Ticket Desk Application");
+        System.out.println("=".repeat(50));
 
+        // display main menu and proccec user choise
+        while (count){
+            System.out.println("\nMain menu >> Choose user type:");
+            System.out.println("(1) Admin\n(2) Customer\n(3) Exit");
+            System.out.print("\nEnter your user type: ");
+            choice = inputChoice.nextLine();
 
-        // proccec user choise
+            if (choice.equals("1")){
+                adminHandler();
+            }
+
+            else if (choice.equals("2")){
+                customerHandler();
+            }
+
+            else if (choice.equals("3")){
+                count = false;
+                System.out.println("Thank you for using this program ...GodBay!");
+            }
+            
+            else {
+                System.out.println("Invalid Input .. Please Try Again >>");
+            }
+        }
         
     }
 
-    // custumer menu handler
+    // customer menu handler
+    static void customerHandler(){
+
+    }
 
 
     // admin menu handler
+    static void adminHandler(){
+
+    }
 
     
     // git index numer of a record from arrayList
