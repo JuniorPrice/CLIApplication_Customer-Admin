836545bc67fda67af01b648fe46e39e593ba344a
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-02-16 19:47:37 +0400

    declear variable

--- CLIAppAdminCustomer.java
+++ CLIAppAdminCustomer.java
@@ -1,6 +1,16 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CLIAppAdminCustomer{
     
     // declear variables
+    ArrayList<ArrayList<String>> tickets = new ArrayList<>();
+    String ticketNumber = "";
+    String ticketDescription = "";
+    String ticketPriority = "";
+    Scanner inputChoise = new Scanner(System.in);
+    Scanner inputTicketInfo = new Scanner(System.in);
+    boolean count = true;
 
     public static void main(String[] args) {
 
