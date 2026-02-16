f58eb7e761167ec01f54106b2f1406b7ef625537
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-02-16 22:32:26 +0400

    add Admin handler functionality

--- CLIAppAdminCustomer.java
+++ CLIAppAdminCustomer.java
@@ -5,13 +5,15 @@ public class CLIAppAdminCustomer{
     
     // declear variables
     static ArrayList<ArrayList<String>> tickets = new ArrayList<>();
+    static ArrayList<String> ticket = new ArrayList<>();
     static String ticketNumber = "";
     static String ticketDescription = "";
     static String ticketPriority = "";
     static Scanner inputChoice = new Scanner(System.in);
     static Scanner inputTicketInfo = new Scanner(System.in);
     static String choice = "";
-    static boolean count = true;
+    static boolean countinue = true;
+    static String PIN = "1234";
 
     public static void main(String[] args) {
 
@@ -21,7 +23,7 @@ public class CLIAppAdminCustomer{
         System.out.println("=".repeat(50));
 
         // display main menu and proccec user choise
-        while (count){
+        while (countinue){
             System.out.println("\nMain menu >> Choose user type:");
             System.out.println("(1) Admin\n(2) Customer\n(3) Exit");
             System.out.print("\nEnter your user type: ");
@@ -36,7 +38,7 @@ public class CLIAppAdminCustomer{
             }
 
             else if (choice.equals("3")){
-                count = false;
+                countinue = false;
                 System.out.println("Thank you for using this program ...GodBay!");
             }
             
@@ -55,6 +57,71 @@ public class CLIAppAdminCustomer{
 
     // admin menu handler
     static void adminHandler(){
+        boolean coun = true;
+        System.out.print("Enter PIN: ");
+        String pin = inputChoice.nextLine();
+
+        // authentication administrator access
+        if (!pin.equals(PIN)){
+            System.out.println("You have enterd wrong PIN ...!");
+            System.out.println("Returning to main menu >>\n");
+        }
+        else {
+            while (coun){
+                // display administrator menu
+                System.out.println("\nAdministrator menu >> Choose user type:");
+                System.out.println("(1) View Ticket\n(2) Update Ticket Description\n(3) Back to Main Menu");
+                System.out.print("\nEnter your choice: ");
+                choice = inputChoice.nextLine();
+
+                // process administrator choice
+                // view ticket operation
+                if (choice.equals("1")){
+                    System.out.print("Enter ticket number: ");
+                    ticketNumber = inputTicketInfo.nextLine();
+                    if (gitIndex(tickets, ticketNumber) == -1){
+                        System.out.printf("There is no ticket with (%s) ticket number ...!\n" , ticketNumber);
+                    }
+                    else {
+                        ticket = tickets.get(gitIndex(tickets, ticketNumber));
+                        System.out.println();
+                        System.out.printf("Ticket Number: %s\n" , ticket.get(0));
+                        System.out.printf("Ticket Description: %s\n" , ticket.get(1));
+                        System.out.printf("Ticket Priority: %s\n" , ticket.get(2));
+                    }
+                }
+
+                // update ticket description operation 
+                else if (choice.equals("2")){
+                    System.out.print("Enter ticket number: ");
+                    ticketNumber = inputTicketInfo.nextLine();
+
+                    if (gitIndex(tickets, ticketNumber) == -1){
+                        System.out.printf("There is no ticket with (%s) ticket number ...!\n" , ticketNumber);
+                    }
+
+                    else {
+                        ticket = tickets.get(gitIndex(tickets, ticketNumber));
+                        System.out.println();
+                        System.out.print("Enter New Ticket Description: ");
+                        ticketDescription = inputTicketInfo.nextLine();
+                        ticket.set(1, ticketDescription);
+                        tickets.set(gitIndex(tickets, ticketNumber), ticket);
+                        System.out.println("Ticket Description Updated ..");
+                    }
+
+                }
+
+                // back to the main menu
+                else if (choice.equals("3")){
+                    System.out.println("Back to Main Menu >>>");
+                    coun = false;
+                }
+                else {
+                    System.out.println("Invalid Input .. Please Try Again >>");
+                }
+            }
+        }
 
     }
 
