1a0eb2ca990548b97db99360876746fc8836cfe3
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-02-16 16:38:08 +0400

    prepare the basic program structure

---
+++ CLIAppAdminCustomer.java
@@ -0,0 +1,22 @@
+public class CLIAppAdminCustomer{
+    
+    // declear variables
+
+    public static void main(String[] args) {
+
+        // display main menu and take user input
+
+
+        // proccec user choise
+        
+    }
+
+    // custumer menu handler
+
+
+    // admin menu handler
+
+    
+    // git index numer of a record from arrayList
+
+}
